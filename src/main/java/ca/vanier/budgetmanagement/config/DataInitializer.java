094ce8f5a92096f63d719860cfe3f16437a7775c
Kevin Britten <kevinjbritten@gmail.com>
2025-02-01 17:25:30 -0500

    fix saving of incomes in DataInitializer to pass userId

--- src/main/java/ca/vanier/budgetmanagement/config/DataInitializer.java
+++ src/main/java/ca/vanier/budgetmanagement/config/DataInitializer.java
@@ -84,7 +84,7 @@ public class DataInitializer {
                                                                 LocalDate.now().minusDays(25), BONUS),
                                                 createIncome(2500.00, "Stock dividends", users.get(2),
                                                                 LocalDate.now().minusDays(30), DIVIDEND)));
-                incomes.forEach(list -> list.forEach(incomeService::save));
+                incomes.forEach(list -> list.forEach(income -> incomeService.save(income.getUser().getId(), income)));
         }
 
         private List<ExpenseCategory> addExpenseCategories(List<User> users) {
