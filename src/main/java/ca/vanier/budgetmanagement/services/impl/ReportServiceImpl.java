f59a5a9b36ef320962e0ebf3f8609723c03005b7
Kevin Britten <kevinjbritten@gmail.com>
2025-02-02 14:12:04 -0500

    only update fields on report if not null in updated report object

--- src/main/java/ca/vanier/budgetmanagement/services/impl/ReportServiceImpl.java
+++ src/main/java/ca/vanier/budgetmanagement/services/impl/ReportServiceImpl.java
@@ -30,7 +30,6 @@ public class ReportServiceImpl implements ReportService {
     public Report createReport(Long userId, LocalDate startDate, LocalDate endDate) {
         GlobalLogger.info(ReportService.class, "Creating report for user id: " + userId);
 
-
         Report report = new Report();
 
         try {
@@ -47,18 +46,15 @@ public class ReportServiceImpl implements ReportService {
         List<Income> allIncomes = incomeService.find(
                 userId,
                 startDate,
-                endDate
-        );
+                endDate);
 
         List<Expense> allExpenses = expenseService.find(
                 userId,
                 startDate,
-                endDate
-        );
+                endDate);
 
         report.setIncomes(allIncomes);
         report.setExpenses(allExpenses);
-
 
         double totalIncome = allIncomes.stream()
                 .mapToDouble(Income::getAmount)
@@ -76,7 +72,6 @@ public class ReportServiceImpl implements ReportService {
         return reportRepository.save(report);
     }
 
-
     public Report getReportById(Long id) {
         GlobalLogger.info(ReportService.class, "Getting report by id: " + id);
         return reportRepository.findById(id)
@@ -89,11 +84,18 @@ public class ReportServiceImpl implements ReportService {
         ReportValidator.validateReport(report);
 
         Report existingReport = getReportById(report.getId());
-        existingReport.setStartDate(report.getStartDate());
-        existingReport.setEndDate(report.getEndDate());
-        existingReport.setIncomes(report.getIncomes());
-        existingReport.setExpenses(report.getExpenses());
-
+        if (report.getStartDate() != null) {
+            existingReport.setStartDate(report.getStartDate());
+        }
+        if (report.getEndDate() != null) {
+            existingReport.setEndDate(report.getEndDate());
+        }
+        if (report.getIncomes() != null && !report.getIncomes().isEmpty()) {
+            existingReport.setIncomes(report.getIncomes());
+        }
+        if (report.getExpenses() != null && !report.getExpenses().isEmpty()) {
+            existingReport.setExpenses(report.getExpenses());
+        }
 
         double totalIncome = report.getIncomes().stream()
                 .mapToDouble(Income::getAmount)
@@ -120,7 +122,6 @@ public class ReportServiceImpl implements ReportService {
 
     }
 
-
     public List<Report> getReportsByUserId(Long userId) {
         GlobalLogger.info(ReportService.class, "Getting reports by user id: " + userId);
 
@@ -128,6 +129,5 @@ public class ReportServiceImpl implements ReportService {
                 .orElseThrow(() -> new IllegalArgumentException("User not found"))
                 .getReports();
     }
-
 
 }
